e98c023db96632d1f82153a89705b2f0882058f8
Deepanshu sharma <chuiwaldeepanshu@gmail.com>
2025-05-17 13:28:37 +0530

    maded below section

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,23 +2,37 @@ import React from 'react'
 import './about.css'
 
 function About() {
-  return (
-    <div className="aa">
-        <div className="div1about">
-            <div className="divabout11"></div>
-            <div className="divabout12"></div>
+    return (
+        <div className="aa">
+            <div className="div1about">
+                <div className="divabout11"></div>
+                <div className="divabout12"></div>
+            </div>
+            <div className="div2about">
+                <div className="divabout21">
+                    <div className="abouttext">
+                        <h1>Data Center Management Software</h1>
+                        <p>
+                            Increase uptime, reduce costs, automate operations, and stay compliant.</p>
+                            <p>Built for colocations, data centers, and cloud providers.</p>
+
+
+
+                    </div>
+                </div>
+                <div className="divabout22">
+                    <button className='buttonrequest
+                    '>Request Demo</button>
+
+                </div>
+            </div>
+            <div className="div1about">
+                <div className="divabout11"></div>
+                <div className="divabout12"></div>
+            </div>
         </div>
-        <div className="div2about">
-            <div className="divabout21"></div>
-            <div className="divabout22"></div>
-        </div>
-        <div className="div1about">
-            <div className="divabout11"></div>
-            <div className="divabout12"></div>
-        </div>
-    </div>
-  
-  )
+
+    )
 }
 
 export default About
